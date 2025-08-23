67b840e73b14ff983421eb1c6a7d345c505ed536
DubeyHarshitt <harshitdubey02004@gmail.com>
2025-08-23 21:27:53 +0530

    created gitCopy

---
+++ server/envCopy.js
@@ -0,0 +1,7 @@
+MONGODB_URI="mongodb+srv://harshit:<Pass></Pass>@cluster0.f4fuub8.mongodb.net/"
+PORT=3000
+GOOGLE_CLIENT_ID="18980118672-bvs10a6seq1qgv2reuj7erelskft59.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET="GOCSPX-jkTmwcLmNqfgi8uwdrDptSl_dd"
+
+JWT_SECRET="Examock"
+// REFRESH_TOKEN_SECRET="Examock"
